8edead70a882bbb20cfee7b926f4aa935e6da5c5
ABBAS <abbasm559@gmail.com>
2018-09-06 00:45:04 +0600

    worked on radio buttons

--- app/src/main/java/com/example/real/thinkers/thinkweather/MainActivity.java
+++ app/src/main/java/com/example/real/thinkers/thinkweather/MainActivity.java
@@ -6,7 +6,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.real.thinkers.thinkweather.models.Weather;
 
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     Button refreshButton;
     String cityName;
     String url;
+    RadioButton radioFahrenheit,radioCentigrde;
     EditText inputCity;
     TextView city, date, temperature, conditions;
     @Override
@@ -32,19 +35,34 @@ public class MainActivity extends AppCompatActivity {
         temperature =  findViewById(R.id.temperature);
         conditions =  findViewById(R.id.conditions);
         inputCity = findViewById(R.id.editTextInput);
-
+        radioFahrenheit= findViewById(R.id.fahrenheit);
+        radioCentigrde= findViewById(R.id.centigrade);
+        radioFahrenheit.setChecked(true);
+        radioFahrenheit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               change();
+            }
+        });
+        radioCentigrde.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                change();
+            }
+        });
 
         refreshButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        cityName=inputCity.getText().toString();
+                            cityName = inputCity.getText().toString();
                         url = "yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+cityName
                                 +"%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
                         change();
                     }
                 }
         );
+
     }
 
     @Override
@@ -57,10 +75,19 @@ public class MainActivity extends AppCompatActivity {
         WeatherApiInterface.Factory.getInstance().getWeather(url).enqueue(new Callback<Weather>() {
             @Override
             public void onResponse(Call<Weather> call, Response<Weather> response) {
-                temperature.setText(response.body().getQuery().getResults().getChannel().getItem().getCondition().getTemp());
-                city.setText(response.body().getQuery().getResults().getChannel().getLocation().getCity());
-                date.setText(response.body().getQuery().getResults().getChannel().getLastBuildDate());
-                conditions.setText(response.body().getQuery().getResults().getChannel().getItem().getCondition().getText());
+                String cityName= response.body().getQuery().getResults().getChannel().getLocation().getCity();
+                String buildDate= response.body().getQuery().getResults().getChannel().getLastBuildDate();
+                String condition= response.body().getQuery().getResults().getChannel().getItem().getCondition().getText();
+
+                if (radioFahrenheit.isChecked()){
+                   String temperatureCount = response.body().getQuery().getResults().getChannel().getItem().getCondition().getTemp();
+                    temperature.setText(temperatureCount);
+                }else if (radioCentigrde.isChecked()){
+                }
+                city.setText(cityName);
+                date.setText(buildDate);
+                conditions.setText(condition);
+
             }
             @Override
             public void onFailure(Call<Weather> call, Throwable t) {
