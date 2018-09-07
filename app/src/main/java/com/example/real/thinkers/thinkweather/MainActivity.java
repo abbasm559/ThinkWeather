d6320313ce7fa0cba95e0e5187367da88776fede
ABBAS <abbasm559@gmail.com>
2018-09-07 23:40:26 +0600

    Completed Current weather and autocompleteTextView
    waiting for forcast.......................plzzzzzzz

--- app/src/main/java/com/example/real/thinkers/thinkweather/MainActivity.java
+++ app/src/main/java/com/example/real/thinkers/thinkweather/MainActivity.java
@@ -4,26 +4,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.real.thinkers.thinkweather.models.Weather;
+
+import java.text.DecimalFormat;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    String [] cities= {"Dhaka", "Chittagong", "Barisal", "Klulna", "Rajshahi", "Rangpur", "Sylhet", "Mymensingh", "Comilla", "New Delhi",
+            "Islamabad", "Kabul", "Yerevan", "Baku", "Manama", "Kuwait City", "Thimphu", "Bandar Seri Begawan", "Phnom Penh", "Beijing",
+            "Nicosia", "Tbilisi", "Jakarta", "Tehran", "Baghdad", "Jerusalem", "Tokyo", "Amman", "Astana", "Bishkek", "Vientiane", "Beirut", "Kuala Lumpur",
+            "Male", "Kathmandu", "Muscat", "Jerusalem (East)", "Doha", "Moscow", "Riyadh"};
     private String TAG = "MainActivity";
     Button refreshButton;
-    String cityName;
+    String cityName="Dhaka";
     String url;
+    String temperatureCount;
     RadioButton radioFahrenheit,radioCentigrde;
-    EditText inputCity;
-    TextView city, date, temperature, conditions;
+    AutoCompleteTextView autoCompleteTv;
+    TextView city, date, temperature, conditions,unitsTv;
+    //forecast data---------------------------------------
+    /*TextView nextDay1, nextDay2, nextDay3,nextDay4, nextDay5;
+    TextView nextDay1Max, nextDay2Max, nextDay3Max, nextDay4Max, nextDay5Max;
+    TextView nextDay1Min, nextDay2Min, nextDay3Min, nextDay4Min, nextDay5Min;
+    ImageView nextDay1Img, nextDay2Img,nextDay3Img,nextDay4Img,nextDay5Img;*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,19 +48,40 @@ public class MainActivity extends AppCompatActivity {
         date =  findViewById(R.id.date);
         temperature =  findViewById(R.id.temperature);
         conditions =  findViewById(R.id.conditions);
-        inputCity = findViewById(R.id.editTextInput);
+        autoCompleteTv = findViewById(R.id.autoCompleteTv);
         radioFahrenheit= findViewById(R.id.fahrenheit);
         radioCentigrde= findViewById(R.id.centigrade);
+        unitsTv= findViewById(R.id.units);
+
+        autoCompleteTv=findViewById(R.id.autoCompleteTv);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,cities);
+        autoCompleteTv.setAdapter(adapter);
+        autoCompleteTv.setThreshold(1);
+
+
+        //forecast data introducing--------------------------------------------
+        /*nextDay1 = findViewById(R.id.nextDay1); nextDay1Max = findViewById(R.id.maxTempNextDay1);
+        nextDay2 = findViewById(R.id.nextDay2); nextDay2Max = findViewById(R.id.maxTempNextDay2);
+        nextDay3 = findViewById(R.id.nextDay3); nextDay3Max = findViewById(R.id.maxTempNextDay3);
+        nextDay4 = findViewById(R.id.nextDay4); nextDay4Max = findViewById(R.id.maxTempNextDay4);
+        nextDay5 = findViewById(R.id.nextDay5); nextDay5Max = findViewById(R.id.maxTempNextDay5);
+        nextDay1Min = findViewById(R.id.minTempNextDay1);   nextDay1Img = findViewById(R.id.nextDayImg1);
+        nextDay2Min = findViewById(R.id.minTempNextDay2);   nextDay2Img = findViewById(R.id.nextDayImg2);
+        nextDay3Min = findViewById(R.id.minTempNextDay3);   nextDay3Img = findViewById(R.id.nextDayImg3);
+        nextDay4Min = findViewById(R.id.minTempNextDay4);   nextDay4Img = findViewById(R.id.nextDayImg4);
+        nextDay5Min = findViewById(R.id.minTempNextDay5);   nextDay5Img = findViewById(R.id.nextDayImg5);*/
+
+
         radioFahrenheit.setChecked(true);
-        radioFahrenheit.setOnClickListener(new View.OnClickListener() {
+        radioFahrenheit.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-               change();
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                change();
             }
         });
-        radioCentigrde.setOnClickListener(new View.OnClickListener() {
+        radioCentigrde.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 change();
             }
         });
@@ -55,11 +90,14 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                            cityName = inputCity.getText().toString();
+                            cityName = autoCompleteTv.getText().toString();
+                            if (cityName.equals("")){
+                                autoCompleteTv.setError("Please input a city");
+                            }else {
                         url = "yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+cityName
                                 +"%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
                         change();
-                    }
+                    }}
                 }
         );
 
@@ -79,14 +117,30 @@ public class MainActivity extends AppCompatActivity {
                 String buildDate= response.body().getQuery().getResults().getChannel().getLastBuildDate();
                 String condition= response.body().getQuery().getResults().getChannel().getItem().getCondition().getText();
 
+
                 if (radioFahrenheit.isChecked()){
-                   String temperatureCount = response.body().getQuery().getResults().getChannel().getItem().getCondition().getTemp();
+                   temperatureCount = response.body().getQuery().getResults().getChannel().getItem().getCondition().getTemp();
                     temperature.setText(temperatureCount);
+                    unitsTv.setText("°F");
                 }else if (radioCentigrde.isChecked()){
+                    double tempInDouble = Double.parseDouble(temperatureCount);
+                    double tempInCentigrade = (tempInDouble-40.0)*(100.0/180.0);
+                    DecimalFormat df = new DecimalFormat("#.#");
+                    String tempInDecimal = df.format((tempInCentigrade));
+                    temperature.setText(tempInDecimal);
+                    unitsTv.setText("°C");
                 }
+
                 city.setText(cityName);
                 date.setText(buildDate);
                 conditions.setText(condition);
+
+
+
+                //forecast data set up-----------------------------------------
+
+               // nextDay1.setText(response.body().getQuery().getResults().getChannel().getItem().getForecast().lastIndexOf(0));
+
 
             }
             @Override
@@ -95,4 +149,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-}
+
+
+    }
+
--- app/src/main/java/com/example/real/thinkers/thinkweather/models/Forecast.java
+++ app/src/main/java/com/example/real/thinkers/thinkweather/models/Forecast.java
@@ -25,6 +25,7 @@ public class Forecast {
     @Expose
     private String text;
 
+
     /**
      * 
      * @return
